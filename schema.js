c6cf97312f185574db8cb1d9db1ac03b9ed216a6
Gabe Marquez <gabemarquez@protonmail.com>
2020-01-30 16:31:34 -0500

    server things

--- schema.js
+++ schema.js
@@ -1,6 +1,6 @@
-import { gql } from 'apollo-server'
+import gql from 'graphql-tag'
 
-export const PlotterTypes = gql`
+export const PlotterTypeDefs = gql`
 type Coordinate {
   latitude: Float!
   longitude: Float!
@@ -16,30 +16,35 @@ enum Day {
   SUNDAY
 }
 
-type DateTime {
+type Date {
   day: Day
-  timestamp: Date
+  weirdTimestamp: Float
+  timestamp: Int
 }
 
 type PlotterPoint {
   location: Coordinate!
-  time: DateTime!
+  time: Date!
 }
 
 type PlotterData {
   points: [PlotterPoint]
-}
-`
+}`
 
-export const PlotterQueries = gql`type Query{
-  locations: [PlotterPoint]
+export const PlotterQueries = gql`
+type Query{
+  locations(start: Date, end: Date): [PlotterPoint]
   day(day: Day): [PlotterPoint]
   interval(start: Date!, end: Date!): [PlotterPoint]
 }`
 
-export const PlotterMutations = gql`type Mutation {
-  addLocation(longitude: Float!, latitude: Float!, when: timestamp): PlotterPoint
+export const PlotterMutations = gql`
+type Mutation {
+  addLocation(longitude: Float!, latitude: Float!, when: Date): PlotterPoint
 }`
 
 export const PlotterResolvers = {
+  Query: {
+    locations:
+  }
 }
--- server.js
+++ server.js
@@ -1,41 +1,29 @@
 import dotenv from 'dotenv'
 import Debug from 'debug'
 
-import { PlotterTypeDefs, PlotterQueries, PlotterMutations } from './schema.js'
 const debug = Debug('plotter:apollo-server')
 dotenv.config()
 
 
 // Connect to MongoDB
 
-import mongoose from 'mongoose'
-
-const url = ;
-
-mongoose.connect(
-  process.env.PLOTTER_DB_URL || 'mongodb://localhost:27017/plotterdb',
-  { useNewUrlParser: true }
-)
-
-db.on('error', console.error.bind(console, 'connection error:'));
-
-const resolvers = {
-  PlotterPoint: {
-    dates: () => {
-      // return dates
-      return 'turn this string into arrays of dates'
-    }
-  }
-}
-
-debug({ resolvers })
-
 import apollo from 'apollo-server'
 const { ApolloServer } = apollo
+import { PlotterTypeDefs, PlotterQueries, PlotterMutations } from './schema.js'
 
 const server = new ApolloServer({
-  typeDefs: [PlotterTypeDefs, PlotterQuery, PlotterMutations],
-  resolvers
+  typeDefs: [
+    PlotterTypeDefs,
+    PlotterQueries,
+    PlotterMutations
+  ],
+  resolvers: [
+    PlotterResolvers
+  ],
+  context: ({ req, payload }) => ({
+    ...req,
+    mongoModels
+  })
 })
 
 server.listen().then(({ url }) => {
