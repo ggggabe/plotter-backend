99a00f5c9e7d7f39ddb622415b3c78700cd590a1
Gabe Marquez <gabemarquez@protonmail.com>
2020-02-01 18:15:56 -0500

    clean up

--- mongodb/config.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose'
-mongoose.Promise = global.Promise
-
-const url = 'mongodb://localhost:27017/plotterdb';
-
-mongoose.connect(url, { useNewUrlParser: true });
-mongoose.connection.once('open', () => console.log(`Connected to mongo at ${url}`));
